63edf353153f88043841913404838dd18999ac8b
Virendra <patelvirendra62@gmail.com>
2020-03-29 19:32:48 +0530

    lecture 5

--- src/feelfreetocode/controllers/MainController.java
+++ src/feelfreetocode/controllers/MainController.java
@@ -1,6 +1,8 @@
 package feelfreetocode.controllers;
 
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Tab;
 
 import java.net.URL;
@@ -12,7 +14,14 @@ public class MainController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        try{
+            Node node = FXMLLoader.load(getClass().getResource("../fxmls/tableviewfxml.fxml"));
+            tableviewTab.setContent(node);
+        }catch (Exception ex){
 
+            ex.printStackTrace();
+            System.out.println("File Not Found..");
+        }
     }
 
 }
---
+++ src/feelfreetocode/controllers/TableviewController.java
@@ -0,0 +1,13 @@
+package feelfreetocode.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TableviewController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
