4986cdac2473d4b72df103eec65d515b5ba3a8c7
Virendra <patelvirendra62@gmail.com>
2020-03-30 00:48:27 +0530

    lecture  11

--- src/feelfreetocode/controllers/DashboardController.java
+++ src/feelfreetocode/controllers/DashboardController.java
@@ -2,9 +2,12 @@ package feelfreetocode.controllers;
 
 import feelfreetocode.models.Case;
 import feelfreetocode.models.DataManager;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.ChoiceBox;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -12,17 +15,45 @@ import java.util.ResourceBundle;
 
 public class DashboardController implements Initializable {
     public BarChart barchart;
+    public ChoiceBox choicebox;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        addChoice();
+        loadChart("Confirmed");
+    }
+
+    private void loadChart(String valueType){
+        this.barchart.getData().clear();
         ArrayList<Case> cases = new DataManager().getData();
         XYChart.Series<String , Integer> series = new XYChart.Series<>();
         for(int i = 0 ; i < cases.size() ; i ++){
             Case cs = cases.get(i);
-            series.getData().add(new XYChart.Data<>(cs.getState() , cs.getTotalConfirmed()));
+            Integer value = -1;
+            if(valueType == "Confirmed")
+                value = cs.getTotalConfirmed();
+            if(valueType == "Cured")
+                value = cs.getCured();
+            if(valueType == "Deaths")
+                value = cs.getDeaths();
+            series.getData().add(new XYChart.Data<>(cs.getState() , value));
 
         }
 
         barchart.getData().add(series);
+    }
+
+    private void addChoice(){
+        this.choicebox.getItems().addAll("Confirmed" , "Cured" , "Deaths");
+
+        this.choicebox.getSelectionModel()
+                .selectedItemProperty()
+                .addListener(new ChangeListener() {
+                    @Override
+                    public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                        String selected = choicebox.getValue().toString();
+                        loadChart(selected);
+                    }
+                });
     }
 }
